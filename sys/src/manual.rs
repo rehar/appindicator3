69f44b92173702a434241c4c2d024d8402ade48c
Rene Harder <rehar@saweb.de>
2022-03-19 09:10:12 -0500

    Regenrated sys package

--- sys/src/manual.rs
+++ sys/src/manual.rs
@@ -1,6 +1,5 @@
 use glib::GType;
 
-#[link(name = "appindicator3")]
 extern "C" {
     // Enum related type function which are getting generated at compile time of libappindicator3
     pub fn app_indicator_category_get_type() -> GType;
